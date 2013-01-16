92c646ee8679ea07a070f2ed80038ac9763de89d
edevaldo batista de souza junior <ed_batista.junior@hotmail.com>
2013-01-16 10:02:11 -0300

    Topo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("header.php") ?>
+<?php include("topo.php") ?>
+    <title>Faz e Acontece</title>
+  </head>
+  <body>
+    <div id="content">
+    </div>
+    <div id="navigation">
+    </div>
+    <div id="sidebar">
+    </div>
+    <div id="footer">
+    <?php $data=date("Y"); echo $data; ?> <!-- Data Atual -->
+    </div>
+  </body>
+</html>
+
---
+++ topo.php
@@ -0,0 +1,24 @@
+
+	    <div id="header">
+		    	<div id="topo" class="centraliza">
+		        	<img src="<?php servidor(); ?>images/logo.png" alt="" id="logo"> 
+
+		        	<div id="contato_topo"></div> 
+
+			        	<div id="redes" class="centraliza">    
+				        	<a href=""><img src="<?php servidor(); ?>images/face_off.png" alt="" id="rede_logo" class="botao"></a>
+				        	<a href=""><img src="<?php servidor(); ?>images/twee_off.png" alt="" id="rede_logo" class="botao"></a>
+			            </div>
+		        </div>
+	        
+		    <div id="menu_topo" class="centraliza"> 
+	        	<!-- <ul>
+		        	<li><a href="<?php servidor(); ?>index"><img src="<?php servidor(); ?>images/topo/menu/home_off.png" alt="" class="botao" /></a></li>
+		        	<li><a href="<?php servidor(); ?>empresa"><img src="<?php servidor(); ?>images/topo/menu/empresa_off.png" alt="" class="botao" /></a></li>
+		        	<li><a href="<?php servidor(); ?>produtos"><img src="<?php servidor(); ?>images/topo/menu/produtos_off.png" alt="" class="botao" /></a></li>
+		        	<li><a href="<?php servidor(); ?>cursos"><img src="<?php servidor(); ?>images/topo/menu/cursos_off.png" alt="" class="botao" /></a></li>
+		        	<li><a href="<?php servidor(); ?>ambientacao"><img src="<?php servidor(); ?>images/topo/menu/ambientacao_off.png" alt="" class="botao" /></a></li>
+		        	<li><a href="<?php servidor(); ?>contato"><img src="<?php servidor(); ?>images/topo/menu/contato_off.png" alt="" class="botao" /></a></li>
+		        </ul> -->
+		    </div>
+	   	</div>
