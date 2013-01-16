246fc1584ae438bd76ad1628ebc80e09914ed163
edevaldo batista de souza junior <ed_batista.junior@hotmail.com>
2013-01-16 11:55:26 -0300

    Footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+	    <div id="footer">		    	
+			<div id="menu_footer" class="centraliza"> 					
+		        <!-- <ul class="centraliza">
+			        <li><a href="<?php servidor(); ?>index"><img src="<?php servidor(); ?>images/menu_topo/home_off.png" alt="" class="botao" /></a></li>
+
+			        <li><a href="<?php servidor(); ?>quem-somos"><img src="<?php servidor(); ?>images/menu_topo/quemsomos_off.png" alt="" class="botao" /></a></li>
+
+			        <li><a href="<?php servidor(); ?>galerias"><img src="<?php servidor(); ?>images/menu_topo/galerias_off.png" alt="" class="botao" /></a></li>
+
+			        <li><a href="<?php servidor(); ?>clientes"><img src="<?php servidor(); ?>images/menu_topo/clientes_off.png" alt="" class="botao" /></a></li>
+
+			        <li><a href="<?php servidor(); ?>noticias"><img src="<?php servidor(); ?>images/menu_topo/noticias_off.png" alt="" class="botao" /></a></li>
+
+			    	<li><a href="<?php servidor(); ?>agenda"><img src="<?php servidor(); ?>images/menu_topo/agenda_off.png" alt="" class="botao" /></a></li>
+			    </ul> --> 
+		    </div>
