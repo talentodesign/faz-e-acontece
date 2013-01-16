9c040837bbbf4f67aec334f00b8fa61e023b0f0e
edevaldo batista de souza junior <ed_batista.junior@hotmail.com>
2013-01-16 11:04:37 -0300

    Topo continuação

--- topo.php
+++ topo.php
@@ -4,21 +4,24 @@
 		        	<img src="<?php servidor(); ?>images/logo.png" alt="" id="logo"> 
 
 		        	<div id="contato_topo"></div> 
-
-			        	<div id="redes" class="centraliza">    
+						<div id="redes" class="centraliza">    
 				        	<a href=""><img src="<?php servidor(); ?>images/face_off.png" alt="" id="rede_logo" class="botao"></a>
 				        	<a href=""><img src="<?php servidor(); ?>images/twee_off.png" alt="" id="rede_logo" class="botao"></a>
-			            </div>
-		        </div>
-	        
-		    <div id="menu_topo" class="centraliza"> 
-	        	<!-- <ul>
-		        	<li><a href="<?php servidor(); ?>index"><img src="<?php servidor(); ?>images/topo/menu/home_off.png" alt="" class="botao" /></a></li>
-		        	<li><a href="<?php servidor(); ?>empresa"><img src="<?php servidor(); ?>images/topo/menu/empresa_off.png" alt="" class="botao" /></a></li>
-		        	<li><a href="<?php servidor(); ?>produtos"><img src="<?php servidor(); ?>images/topo/menu/produtos_off.png" alt="" class="botao" /></a></li>
-		        	<li><a href="<?php servidor(); ?>cursos"><img src="<?php servidor(); ?>images/topo/menu/cursos_off.png" alt="" class="botao" /></a></li>
-		        	<li><a href="<?php servidor(); ?>ambientacao"><img src="<?php servidor(); ?>images/topo/menu/ambientacao_off.png" alt="" class="botao" /></a></li>
-		        	<li><a href="<?php servidor(); ?>contato"><img src="<?php servidor(); ?>images/topo/menu/contato_off.png" alt="" class="botao" /></a></li>
-		        </ul> -->
-		    </div>
-	   	</div>
+			            </div>			    
+			   	</div>
+		</div> 
+				<div id="menu_topo"> 					
+		        	<ul class="centraliza">
+			        	<li><a href="<?php servidor(); ?>index"><img src="<?php servidor(); ?>images/menu_topo/home_off.png" alt="" class="botao" /></a></li>
+
+			        	<li><a href="<?php servidor(); ?>quem-somos"><img src="<?php servidor(); ?>images/menu_topo/quemsomos_off.png" alt="" class="botao" /></a></li>
+
+			        	<li><a href="<?php servidor(); ?>galerias"><img src="<?php servidor(); ?>images/menu_topo/galerias_off.png" alt="" class="botao" /></a></li>
+
+			        	<li><a href="<?php servidor(); ?>clientes"><img src="<?php servidor(); ?>images/menu_topo/clientes_off.png" alt="" class="botao" /></a></li>
+
+			        	<li><a href="<?php servidor(); ?>noticias"><img src="<?php servidor(); ?>images/menu_topo/noticias_off.png" alt="" class="botao" /></a></li>
+
+			        	<li><a href="<?php servidor(); ?>agenda"><img src="<?php servidor(); ?>images/menu_topo/agenda_off.png" alt="" class="botao" /></a></li>
+			        </ul> 
+		    	</div>
