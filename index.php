05c08c322087d92e8be1fed679551fd0a97af97a
edevaldo batista de souza junior <ed_batista.junior@hotmail.com>
2013-01-21 11:24:32 -0300

    Index - Contato

--- index.php
+++ index.php
@@ -16,29 +16,23 @@
 
         <div id="area_destaque">
           <div id="menu">
+
+            <?php 
+            $ul = 1;
+            while ( $ul <= 03) {  ?>       
                 <ul>         
                   <li>
-                    <div class="data"> DATA <br/> DATA </div>
-                    <div class="titulo"> TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO </div>
-                    <div class="separator"></div>                   
-                  </li>
-                </ul>   
-                <hr>             
-                <ul>         
-                  <li>
-                    <div class="data"> DATA <br/> DATA </div>
-                    <div class="titulo"> TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO </div>
-                    <div class="separator"></div>                   
-                  </li>
-                </ul>
-                <hr>
-                <ul>         
-                  <li>
-                    <div class="data" class="centraliza"> DATA <br/> DATA </div>
+                    <div class="data"> 
+                      <p class="d"> 03 </p>
+                      <br/>
+                      <p class="m"> nov </p> 
+                    </div>
                     <div class="titulo"> TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO TITULO </div>
                     <div class="separator"></div>                   
                   </li>
                 </ul>
+                <hr>      
+            <?php $ul++; }  ?>
               <center><a href="<?php servidor(); ?>noticias/"><img src="<?php servidor(); ?>images/not_bt_off.png" alt="" class="botao"></a></center>
           </div> 
         </div>
@@ -46,7 +40,19 @@
     </div>
 
     <div id="area_home_principal">
+
+      <div id="area_noticias" class="centraliza">
+        <div id="link_conheca">
+          <img src="<?php servidor(); ?>images/title_conheca.png" alt="">
+          <p>A FAZ E ACONTECE, empresa especializada na organização de eventos, que atua no mercado com experiência e profissionalismo de qualidade. </p>
+          <a href=""><img src="<?php servidor(); ?>images/saiba_mais_off.png" alt="" class="botao" id="nav_conheca"></a>
+        </div>
+      </div>
       
+      <div id="area_contato">
+        
+        
+      </div>
 
     </div>
 
