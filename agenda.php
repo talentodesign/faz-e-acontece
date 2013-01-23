c24e89fc044df4401822cdb14888cd42377e08e7
edevaldo batista de souza junior <ed_batista.junior@hotmail.com>
2013-01-23 11:49:56 -0300

    Agenda

---
+++ agenda.php
@@ -0,0 +1,53 @@
+<?php include("header.php") ?>
+<?php include("topo.php") ?>
+
+    <title>Home</title>
+  </head>
+  <body>
+    <div id="area_title">
+        <div id="content_title" class="centraliza">
+          <div id="title">
+            <img src="<?php servidor(); ?>images/agenda/title.png" alt="">
+            <br>
+            <img src="<?php servidor(); ?>images/agenda/sub_title.png" alt="">
+          </div>
+        </div>
+    </div>
+
+      <div id="content">
+        <div id="pag_agenda" class="centraliza">
+
+          <div id="agenda">
+            <img src="<?php servidor(); ?>images/agenda/ano.png" alt="">
+            <ul>
+              <li><a href=""><p>JAN</p></a></li>
+              <li><a href=""><p>FEV</p></a></li>
+              <li><a href=""><p>MAR</p></a></li>
+              <li><a href=""><p>ABR</p></a></li>
+              <li><a href=""><p>MAI</p></a></li>
+              <li><a href=""><p>JUN</p></a></li>
+              <li><a href=""><p>JUL</p></a></li>
+              <li><a href=""><p>AGO</p></a></li>
+              <li><a href=""><p>SET</p></a></li>
+              <li><a href=""><p>OUT</p></a></li>
+              <li><a href=""><p>NOV</p></a></li>
+              <li><a href=""><p>DEZ</p></a></li> 
+            </ul>
+          </div>
+
+          <div id="agenda_info">
+
+          
+          </div> 
+        </div>       
+      </div>
+    
+
+
+
+  
+    <div id="footer">
+    <?php include("footer.php") ?>
+    </div>
+  </body>
+</html>
